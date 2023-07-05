657a222976a75549eeb179b4a169aed77e14b475
Pham Tien Dung (HN-BSD-Dev <dungpt1@runsystem.local>
2023-07-05 16:40:30 +0700

    udpate

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [objectData, setObjectData] = useState({});
+
+  useEffect(() => {
+    const ws = new WebSocket('ws://localhost:8080');
+
+    ws.onopen = () => {
+      console.log('Connected to WebSocket server');
+    };
+
+    ws.onmessage = (event) => {
+      const message = event.data;
+
+      try {
+        const updatedObjectData = JSON.parse(message);
+        setObjectData(updatedObjectData);
+      } catch (error) {
+        console.error('Error parsing JSON:', error);
+      }
+    };
+
+    ws.onclose = () => {
+      console.log('Disconnected from WebSocket server');
+    };
+
+    return () => {
+      ws.close();
+    };
+  }, []);
+
+  const makeid = (length) => {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+  }
+
+
+  const handleClick = () => {
+    const updatedObjectData = { ...objectData };
+    const ramdomText = makeid(20);
+    updatedObjectData.ramdomText = ramdomText
+    const ws = new WebSocket('ws://localhost:8080');
+
+    ws.onopen = () => {
+      console.log('Connected to WebSocket server');
+      ws.send(JSON.stringify(updatedObjectData));
+    };
+
+    ws.onmessage = (event) => {
+      const message = event.data;
+
+      try {
+        const updatedObjectData = JSON.parse(message);
+        setObjectData(updatedObjectData);
+      } catch (error) {
+        console.error('Error parsing JSON:', error);
+      }
+    };
+
+    ws.onclose = () => {
+      console.log('Disconnected from WebSocket server');
+    };
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Update Object Data</button>
+      <pre>{JSON.stringify(objectData, null, 2)}</pre>
+    </div>
+  );
+}
+
+export default App
